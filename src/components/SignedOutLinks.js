ca9b5a096742a912ea8894cd90af108c1fe7e761
Shomari Pope <shomaripope@gmail.com>
2019-08-29 21:42:18 -0400

    got most everything else working and submitting to firebase. working on some css and adding more content

--- src/components/SignedOutLinks.js
+++ src/components/SignedOutLinks.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Logo from '../images/facebook2.png';
+import Logo2 from '../images/instagram.png';
 
 const SignedInLinks = () => {
     return (
+        <div>
                 <ul >
                 <li><NavLink to='/' className="btn waves-effect waves-light blue lighten-1"> Home </NavLink></li>    
                 {/* <li><NavLink to='/signin' className="btn waves-effect waves-light blue lighten-1">Admin Login </NavLink></li> */}
@@ -10,6 +13,16 @@ const SignedInLinks = () => {
                 <li><NavLink to='/events' className="btn waves-effect waves-light blue lighten-1"> Upcoming Events </NavLink></li>
                 <li><NavLink to='/requestevent' className="btn waves-effect waves-light blue lighten-1"> Book An Event </NavLink></li>
                 </ul>
+                <ul>
+                    <li className="right">
+                    <a href="https://www.facebook.com/1ststopminimart/" ><img src={Logo} alt="logo" id="socialIcon" /></a>
+                    </li>
+                    <li className="right">
+                    <a href="https://www.instagram.com/1st_stopminimart/" ><img src={Logo2} alt="logo" id="socialIcon" /></a>
+                    </li>
+                </ul>
+                
+        </div>
     )
 }
 
